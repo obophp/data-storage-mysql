89fdfab3b8e8c08ca1651d860eb03d37f8389df8
Filip Siwiec <filip.siwiec@enwoxsystems.cz>
2017-09-14 13:12:36 +0200

    Passing property information to convertors

--- tests/DefaultMysqlDataConverterTest.php
+++ tests/DefaultMysqlDataConverterTest.php
@@ -11,18 +11,29 @@ require __DIR__ . "/bootstrap.php";
  */
 class DataConverterTest extends \Tester\TestCase {
 
+    /**
+     * @var \obo\Carriers\PropertyInformationCarrier
+     */
+    public $propertyInformation;
+
+    public function setUp() {
+        parent::setUp();
+        $this->propertyInformation = new \obo\Carriers\PropertyInformationCarrier();
+    }
+
     public function testDeserializeArray() {
         $dataConverter = new \obo\DataStorage\Tests\Assets\DefaultMysqlDataConverter();
 
-        Assert::equal(["foo" => "bar"], $dataConverter::deserializeToArray('a:1:{s:3:"foo";s:3:"bar";}'));
+        Assert::equal(["foo" => "bar"], $dataConverter::deserializeToArray('a:1:{s:3:"foo";s:3:"bar";}', $this->propertyInformation));
 
-        Assert::equal([], $dataConverter::deserializeToArray(""));
+        Assert::equal([], $dataConverter::deserializeToArray("", $this->propertyInformation));
 
-        Assert::equal([], $dataConverter::deserializeToArray(null));
+        Assert::equal([], $dataConverter::deserializeToArray(null, $this->propertyInformation));
 
-        Assert::exception(function () use ($dataConverter) {
+        $propertyInformation = $this->propertyInformation;
+        Assert::exception(function () use ($dataConverter, $propertyInformation) {
             \error_reporting(\error_reporting() ^ \E_NOTICE);
-            $dataConverter::deserializeToArray("{some;wrong;string}");
+            $dataConverter::deserializeToArray("{some;wrong;string}", $propertyInformation);
             \error_reporting(\E_ALL);
         }, '\obo\Exceptions\Exception');
     }
@@ -30,15 +41,16 @@ class DataConverterTest extends \Tester\TestCase {
     public function testDeserializeObject() {
         $dataConverter = new \obo\DataStorage\Tests\Assets\DefaultMysqlDataConverter();
 
-        Assert::type('stdClass', $dataConverter::deserializeToObject('O:8:"stdClass":0:{}'));
+        Assert::type('stdClass', $dataConverter::deserializeToObject('O:8:"stdClass":0:{}', $this->propertyInformation));
 
-        Assert::equal(null, $dataConverter::deserializeToObject(""));
+        Assert::equal(null, $dataConverter::deserializeToObject("", $this->propertyInformation));
 
-        Assert::equal(null, $dataConverter::deserializeToObject(null));
+        Assert::equal(null, $dataConverter::deserializeToObject(null, $this->propertyInformation));
 
-        Assert::exception(function () use ($dataConverter) {
+        $propertyInformation = $this->propertyInformation;
+        Assert::exception(function () use ($dataConverter, $propertyInformation) {
             \error_reporting(\error_reporting() ^ \E_NOTICE);
-            $dataConverter::deserializeToObject("{some;wrong;string}");
+            $dataConverter::deserializeToObject("{some;wrong;string}", $propertyInformation);
             \error_reporting(\E_ALL);
         }, '\obo\Exceptions\Exception');
     }
